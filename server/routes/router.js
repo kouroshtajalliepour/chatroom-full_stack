613846db60f41f79b0667a65a4de42faa19d6fac
kouroshtajalliepour <65805364+kouroshtajalliepour@users.noreply.github.com>
2020-08-27 08:03:43 -0700

    finished server.js

---
+++ server/routes/router.js
@@ -0,0 +1,4 @@
+const { Router } = require("express");
+
+const express = reqire("express");
+const router = express.Router();
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
+const chatroom = reqiure("./routes/router");
 
 //setting up the express app
 
@@ -13,5 +14,19 @@ const url = "mongodb://localhost:27017/chat-room";
 
 mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => console.log("BOOO"))
+  .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
+
+//middleware
+
+app.use(morgan("dev"));
+
+//static-files 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use express.static((__dirname + '/public/'));
+}
+
+//routs
+
+app.use("", chatroom);
